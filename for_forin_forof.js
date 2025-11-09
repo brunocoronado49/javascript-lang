023004f770a5e2f3ae899903b2d0ec24af4e1456
brunocoronado49 <francorangelcoronado@gmail.com>
2025-11-09 16:17:05 -0600

    Bucles for, for of y for in

--- for_forin_forof.js
+++ for_forin_forof.js
@@ -1,3 +1,17 @@
-for (let index = 0; index < 10; index++) {
-  console.log(index);
+const heroes = ['batman', 'superman', 'spiderman', 'ironman'];
+
+console.log('************* For tradicional *************');
+for (let i = 0; i < heroes.length; i++) {
+  const hero = heroes[i];
+  console.log(`${i} - ${hero}`);
+}
+
+console.log('************* For in *************');
+for (const hero in heroes) {
+  console.log(`${hero} - ${heroes[hero]}`);
+}
+
+console.log('************* For of *************');
+for (const hero of heroes) {
+  console.log(hero);
 }
