4198f4243cad170378bd39b3bb48dbbe9b7f4305
brunocoronado49 <francorangelcoronado@gmail.com>
2025-11-29 10:57:36 -0600

    Empezando a hacer peticiones http

---
+++ http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const BreakingBadApp = (element) => {
+  document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
+  element.innerHTML = 'Loading...';
+}
