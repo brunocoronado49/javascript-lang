da3ed84833a8f37c6f63c753c0c0f017991b95bf
brunocoronado49 <francorangelcoronado@gmail.com>
2025-11-29 18:42:49 -0600

    Mostrando quote y autor

--- http-app/src/breakingbad/breakingbad-app.js
+++ http-app/src/breakingbad/breakingbad-app.js
@@ -1,8 +1,23 @@
 /**
+ * @returns {Promise<Object>} quoteData
+ */
+const fetchQuote = async () => {
+  const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+  const [data] = await response.json();
+  return data;
+}
+
+/**
  *
  * @param {HTMLDivElement} element
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async (element) => {
   document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
   element.innerHTML = 'Loading...';
+  const {quote, author} = await fetchQuote();
+
+  element.innerHTML = `
+    <h1>${quote}</h1>
+    <h2>${author}</h2></br>
+    `;
 }
